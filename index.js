d483654bb60526921a344340b1e2fcadd72b8c7f
Fenil Rokad <fenilrokad3@gmail.com>
2026-09-19 21:30:42 +0530

    created /listings route

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import { Listing } from "./models/listing.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
@@ -9,22 +11,30 @@ const app = express();
 
 const port = 8080;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 async function Main(){
     try{
         //Establishing a connection
         await mongoose.connect(process.env.MONGO_URL);
         console.log(`DataBase connected Succesfully......`);
 
-        const newListing = await Listing.create({
-            title: "Fenil's Place",
-            description: "2BK with attached washroom and connected balcony.",
-            price: 2500,
-            location: "Soul",
-            country: "South Korea"
-        })
+        //root path
+        app.get("/", (req, res) => {
+            res.send("<h1>Welcome to Our page...</h1>")
+        });
 
-        console.log("New Listing added");
-        console.log(newListing);
+        //show all listings
+        app.get("/listings", async (req, res) => {
+            const allListings = await Listing.find();
+
+            // console.log(allListings);
+            res.render("allListings", {allListings});
+        });
 
     }catch(err){
         console.error(`There is an error: ${err}`);
